a64cb4b231904b9482ad69b9e30e145d8fa2685f
MrBotCipher <32300516+RAJANAGORI@users.noreply.github.com>
2023-03-12 15:37:43 +0530

    cewl script

---
+++ cewl.py
@@ -0,0 +1,14 @@
+## This code reads a list of URLs from a file, generates a wordlist for each URL using the cewl command, and prints messages indicating which URL is currently being processed and when the wordlist for each URL has been generated.
+import os
+
+# Open file containing URLs
+with open('C:\\Users\\tnjh0y5iu7\\mywork\\targets\\CYPRESS-W001\\update.txt') as f:
+    # Loop through each URL in the file
+    for url in f:
+        url = url.strip() # Remove newline character
+        # Show which URL is being processed
+        print('Generating wordlist for URL:', url)
+        # Run cewl command to generate wordlist
+        os.system('cewl {} -w {}.txt'.format(url, url.replace('https://', '').replace('www.', '')))
+        # Print message once wordlist is generated
+        print('Wordlist generated for URL:', url)
